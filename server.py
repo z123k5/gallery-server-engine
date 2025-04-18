b8cea068ecb3003273d66f05eab1a28303c1d948
z123k5 <1994918916@qq.com>
2025-04-18 19:47:44 +0800

    Enhance server functionality and integrate Langchain for metadata processing
    - Added Langchain integration for metadata parsing and tag extraction in engine_query_v2.
    - Updated engine_resolve to return selected classes based on image features and tags.
    - Modified middleware to use HTTPS instead of HTTP.
    - Improved image feature extraction and normalization processes.
    - Implemented TensorFlow Lite model for similarity computation in use_model.py.
    - Refactored code for better organization and readability, including dynamic image path retrieval.

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import base64
 import io
 import json
 import os
+from typing import List
 
 from fastapi.responses import JSONResponse, StreamingResponse
 
@@ -25,7 +26,7 @@ from fastapi import FastAPI
 #     get_swagger_ui_oauth2_redirect_html,
 # )
 from fastapi.staticfiles import StaticFiles
-from datetime import datetime
+from datetime import datetime, timedelta
 import sys
 from sqlalchemy.orm import Session
 from src.Database import get_db, get_db_commit
@@ -33,6 +34,16 @@ from sqlalchemy import text
 from src.models import UserScheme
 from sqlite3 import Connection, Cursor
 from src.Logger import logger
+
+# Langchain
+from langchain.chat_models import ChatOpenAI
+from langchain.chains.llm import LLMChain
+from langchain.prompts.prompt import PromptTemplate
+
+
+# Asyncio
+import asyncio
+
 
 logger.info("Server Starting...")
 logger.info("Importing Server Models...")
@@ -65,7 +76,7 @@ app.add_middleware(
 )
 
 
-@app.middleware("http")
+@app.middleware("https")
 async def options_middleware(request: Request, call_next):
     if request.method == "OPTIONS":
         # 返回允许的 HTTP 方法和其他相关头部信息
@@ -147,6 +158,7 @@ async def register(form_data: OAuth2PasswordRequestForm = Depends(), db: Cursor 
 @app.post("/engine/resolve")
 async def engine_resolve(
     file: fastapi.UploadFile = fastapi.File(...),
+    tags: str = fastapi.Form(...),
     current_user: User = Depends(get_current_active_user),
 ):
     img = Image.open(file.file).convert("RGB")
@@ -157,19 +169,45 @@ async def engine_resolve(
         features = image_encoder.get_image_features(**inputs)
         features = features / features.norm(dim=1, keepdim=True)
 
+    """
+    客户端发送一个类别数组，服务端返回这些类别归一化后的特征向量
+    客户端将用于对每个图片打上标签，标签个数根据相似度阈值确定
+    """
+    tags_list = json.loads(tags)
+    # calculate the features from the text batch
+    inputs = text_tokenizer(tags_list, return_tensors="pt", padding=True)['input_ids']
+    with torch.no_grad():
+        features_texts = text_encoder(inputs).logits
+        # Normalize
+        features_texts = features_texts / \
+            features_texts.norm(dim=1, keepdim=True)
+    
+    # cosine similarity
+    logit_scale = image_encoder.logit_scale.exp()
+    logits_per_image = logit_scale * features @ features_texts.t()
+    probs = logits_per_image.softmax(dim=-1).cpu().detach().numpy()
+    probs = probs[0]
+    # select the classes with probability > 0.6
+    selected_classes = []
+    for i in range(len(tags_list)):
+        if probs[i] > 0.6:
+            selected_classes.append(tags_list[i])
+
+    print("Selected Classes: ", selected_classes)
+
     byteArray = features.cpu().numpy().tobytes()
     
     encoded_data = base64.b64encode(byteArray).decode("utf-8")
     # print(encoded_data)
 
-    return {"feat": encoded_data}
-
+    return {"feat": encoded_data, "tags": selected_classes}
 
 @app.get("/engine/query")
 async def engine_query(
     query: str,
     current_user: User = Depends(get_current_active_user),
 ):
+    global logger
     # 1. Encode the query
     inputs = text_tokenizer(query, return_tensors="pt")
     with torch.no_grad():
@@ -179,6 +217,7 @@ async def engine_query(
 
     ret = features.cpu().numpy().tobytes()
     # print("2:sizeof return tensor bytes = ", len(ret))
+    logger.info(f"Query OK: {query}")
 
     return StreamingResponse(
         content=io.BytesIO(ret),
@@ -186,22 +225,79 @@ async def engine_query(
     )
 
 
-# TODO: delte test
-def test_procedure():
-    global test_data
-    # 1. Encode the query
-    inputs = text_tokenizer("123", return_tensors="pt")
+### Preprocessing
+model =  os.environ.get('MODEL', '')
+api_key = os.environ.get('API_KEY', '')
+llm = ChatOpenAI(model_name=model, temperature='0.7',
+                openai_api_key=api_key)
+
+metaParserTemplateStr = """你是一个json解析器，专门解析对查找的照片的信息，你将输入的文本通过理解，若文本没有呈现出某个键的信息，请不要解析这个键，否则解析出键对应的值，并以condensed json形式输出，可选的键有：[date, loc, device]，date:查找照片的日期时间(以今天{date}为参照日期，格式为YYYY-MM-DD HH:MM:SS或者区间YYYY-MM-DD HH:MM:SS~YYYY-MM-DD HH:MM:SS，例如：1."15年到16年的照片"解析为"2015-01-01 00:00:00~2016-01-01 00:00:00"2."去年以前的照片"解析为"1949-01-01 00:00:00~{dateLastYear}"，3."去年的照片"解析为"{dateLastYear}"，4."小时候的照片"由于日期不明确，故不解析date键)，loc:查找照片的地点(例如：1.北京市, 2.武功山)，device:查找照片的设备(如：1.iPhone 13, 2.Nikon zf)，输出格式如下:
+{{date: "<date as you understand it>", loc: "<address as you understand it in china>", device: "<photo device as you understand it>",}}
+输入的文本是："{query}"
+"""
+
+tagParserTemplateStr = """你是一个json解析器，专门解析输入文本中出现的标签，若文本没有呈现出标签，请以`无`作为结果，否则解析出标签，简洁回答，直接输出[tag1, tag2, ...]，可选的标签有：[{tags}]
+输入的文本是："{query}"
+"""
+
+promptParserTemplateStr = """你是一个提示词生成器，专门解析输入文本对应的提示词，若输入文本中含有敏感信息如"时间、地点、拍摄设备"的信息，请做过滤处理，然后输出一个过滤后的信息，不包含以上敏感信息的文本提示词，你不能输出其他无关信息。
+输入的文本是："{query}"
+"""
+
+#####
+
+"""
+客户端发送一个字符串查询，返回一个综合的查询结果
+"""
+@app.get("/engine/query_v2")
+async def engine_query_v2(
+    query: str,
+    tagsAppended: str = "",
+    current_user: User = Depends(get_current_active_user),
+):
+    global logger
+
+    # 1. Search by Meta data
+    # llm resolve metadata
+    metaParserTemplate = PromptTemplate(template=metaParserTemplateStr, input_variables=["date", "dateLastYear", "query"])
+    dateLastYear = datetime.now() - timedelta(days=365)
+    metaParserTemplate = metaParserTemplate.partial(date=datetime.now().strftime("%Y-%m-%d %H:%M:%S"), dateLastYear=dateLastYear)
+    metaParser = LLMChain(llm=llm, prompt=metaParserTemplate,
+                        output_key="out")
+
+    # 2. Get Tags
+    # llm resolve tags
+    tagParserTemplate = PromptTemplate(template=tagParserTemplateStr, input_variables=["tags", "query"])
+    tagParserTemplate = tagParserTemplate.partial(tags=tagsAppended)
+    tagParser = LLMChain(llm=llm, prompt=tagParserTemplate,
+                        output_key="out")
+    # new date, formate:YYYY-MM-DD HH:MM:SS
+
+    # 3. calculate the features
+    # llm resolve prompt
+    promptParserTemplate = PromptTemplate(template=promptParserTemplateStr, input_variables=["query"])
+    promptParser = LLMChain(llm=llm, prompt=promptParserTemplate,
+                        output_key="out")
+    responseMeta, responseTag, responsePrompt = await asyncio.gather(metaParser.ainvoke(query), tagParser.ainvoke(query), promptParser.ainvoke(query))
+
+
+    inputs = text_tokenizer(responsePrompt.out, return_tensors="pt")
     with torch.no_grad():
         features = text_encoder(**inputs).logits
         # Normalize
         features = features / features.norm(dim=1, keepdim=True)
 
-    ret = features.cpu().numpy().tobytes()
-    print("2:sizeof return tensor bytes = ", len(ret))
+    responseFeat = features.cpu().numpy().tobytes()
 
-    test_data = ret
+    # print("2:sizeof return tensor bytes = ", len(ret))
+    logger.info(f"Query OK: {query}")
 
-#####
+    # 4. return to client
+    return JSONResponse(content={
+        "meta": responseMeta.out,
+        "tag": responseTag.out,
+        "feat": base64.b64encode(responseFeat).decode("utf-8")
+    })
 
 
 if __name__ == "__main__":
@@ -231,7 +327,5 @@ if __name__ == "__main__":
 
     logger.info("Loading DataBase...")
 
-    test_procedure()
-
-    uvicorn.run(app, host="0.0.0.0", port=8443) #,
-                # ssl_certfile="./cert/certificate.crt", ssl_keyfile="./cert/private.key")
+    uvicorn.run(app, host="0.0.0.0", port=8443,
+                ssl_certfile="./cert/certificate.crt", ssl_keyfile="./cert/private.key")
--- use_model.py
+++ use_model.py
@@ -8,6 +8,8 @@ from transformers import CLIPProcessor, CLIPModel
 import numpy as np
 from torchsummary import summary
 
+import tensorflow as tf
+
 import os
 os.environ["HF_HOME"] = "./"
 os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
@@ -16,7 +18,21 @@ proxy_dict = {
     "https": "http://127.0.0.1:7890",
 }
 
-query_texts = ['人']  # 这里是输入文本的，可以随意替换。
+# 这里是输入文本的，可以随意替换。
+# <P>
+query_texts = [
+    '一幅精美的插画，展现一片风景如画的湖畔，湖水清澈如镜，倒映着天空中的霞光。湖边盛开着五彩斑斓的鲜花，微风轻拂，花瓣随风飘舞。远处连绵的青山在薄雾中若隐若现，增添了一种梦幻般的氛围。画面整体色调柔和，光影交错，给人一种宁静而唯美的感觉。']
+# <OP>
+# query_texts = [
+#     '''一张破败荒凉的画面，一片枯黄的荒野上，杂草丛生，土地干裂，仿佛许久未曾见过雨水。天空阴沉沉的，被厚重的乌云笼罩，透不出一丝阳光。远处几栋年久失修的废弃建筑残破不堪，窗户破碎，墙壁斑驳脱落，透露出岁月的侵蚀。空气中弥漫着灰尘与寂静，没有任何生机，整个场景透出一股萧条和压抑的氛围，让人感到沉闷而无望。''']
+
+
+
+
+
+# query_texts = [
+#     '一幅富有寓意的绘画，画面中央是一棵历经风霜的古树，它的枝干虽然扭曲苍老，却依然生机勃勃地向天空伸展，象征着坚韧不拔的精神。在树下，一块被风雨侵蚀的顽石静静伫立，表面布满岁月的痕迹，却依然坚固不移，隐喻着坚定不屈的品格。远处，一条潺潺流淌的小溪映照着落日余晖，象征着岁月流转和生命的延续。整幅画面通过自然之物传达深刻的哲理，使人沉思。']
+
 # query_texts = ['啤酒','晚宴','合影','旅游']  # 这里是输入文本的，可以随意替换。
 # 加载Taiyi 中文 text encoder
 text_tokenizer = BertTokenizer.from_pretrained(
@@ -25,19 +41,30 @@ text_encoder = BertForSequenceClassification.from_pretrained(
     "./Taiyi-CLIP-Roberta-large-326M-Chinese").eval()
 text = text_tokenizer(query_texts, return_tensors='pt', padding=True)['input_ids']
 
-url = [
-    "C:/Users/Kang/Desktop/DCIM/temp/_DSC2035.JPG",
-    "C:/Users/Kang/Desktop/DCIM/temp/_DSC2056.JPG",
-    "C:/Users/Kang/Desktop/DCIM/temp/_DSC2807.JPG",
-    "C:/Users/Kang/Desktop/DCIM/temp/_DSC2808.JPG",
-    "C:/Users/Kang/Desktop/DCIM/temp/_DSC2809.JPG",
-    "C:/Users/Kang/Desktop/DCIM/temp/_DSC2844.JPG",
-    "C:/Users/Kang/Desktop/DCIM/temp/_DSC2845.JPG",
-    "C:/Users/Kang/Desktop/DCIM/temp/_DSC2848.JPG",
-    "C:/Users/Kang/Desktop/DCIM/temp/_DSC2851.JPG",
-    "C:/Users/Kang/Desktop/DCIM/temp/_DSC2852.JPG",
-    "C:/Users/Kang/Desktop/DCIM/temp/_DSC2857.JPG",
-]
+# url = [
+#     "C:/Users/Kang/Desktop/DCIM/temp/_DSC2035.JPG",
+#     "C:/Users/Kang/Desktop/DCIM/temp/_DSC2056.JPG",
+#     "C:/Users/Kang/Desktop/DCIM/temp/_DSC2807.JPG",
+#     "C:/Users/Kang/Desktop/DCIM/temp/_DSC2808.JPG",
+#     "C:/Users/Kang/Desktop/DCIM/temp/_DSC2809.JPG",
+#     "C:/Users/Kang/Desktop/DCIM/temp/_DSC2844.JPG",
+#     "C:/Users/Kang/Desktop/DCIM/temp/_DSC2845.JPG",
+#     "C:/Users/Kang/Desktop/DCIM/temp/_DSC2848.JPG",
+#     "C:/Users/Kang/Desktop/DCIM/temp/_DSC2851.JPG",
+#     "C:/Users/Kang/Desktop/DCIM/temp/_DSC2852.JPG",
+#     "C:/Users/Kang/Desktop/DCIM/temp/_DSC2857.JPG",
+# ]
+
+# 从文件夹中获取所有图片的路径
+def get_foldimg_uri():
+    foldimg_uri = []
+    for root, dirs, files in os.walk("C:/Users/Kang/Desktop/DCIM/temp"):
+        for file in files:
+            if file.endswith(".JPG"):
+                foldimg_uri.append(os.path.join(root, file))
+    return foldimg_uri
+
+url = get_foldimg_uri()
 
 # 加载CLIP的image encoder
 clip_model = CLIPModel.from_pretrained("./clip-vit-large-patch14")
@@ -75,4 +102,64 @@ with torch.no_grad():
     print("Logits per image before softmax:", logits_per_image)
 
     probs = logits_per_image.softmax(dim=-1).cpu().numpy()
-    print("Probabilities:", np.around(probs, 3))
+    print("Torch Probabilities:", np.around(probs, 3))
+
+
+    # Calc using Tensorflow
+    # TFlite model
+
+    class SimilarityModel(tf.Module):
+        def __init__(self):
+            super().__init__()
+            self.logit_scale = tf.Variable(logit_scale, dtype=tf.float32)
+
+        @tf.function(input_signature=[
+            tf.TensorSpec(shape=[1, 768], dtype=tf.float32),
+            tf.TensorSpec(shape=[None, 768], dtype=tf.float32)
+        ])
+        def compute_similarity(self, text_feat, image_feat):
+            # text_feat = tf.nn.l2_normalize(text_feat, axis=1)  # 归一化
+            # image_feat = tf.nn.l2_normalize(image_feat, axis=1)  # 归一化
+            logits = logit_scale * (text_feat @ tf.transpose(image_feat))
+            # return tf.nn.softmax(logits, axis=-1)
+            return tf.nn.softmax(logits, axis=-1)
+    
+    # 转换为 TFLite
+    similarity_model = SimilarityModel()
+    converter = tf.lite.TFLiteConverter.from_concrete_functions(
+        [similarity_model.compute_similarity.get_concrete_function()]
+    )
+    # 启用动态形状支持
+    converter.target_spec.supported_ops = [
+        tf.lite.OpsSet.TFLITE_BUILTINS, tf.lite.OpsSet.SELECT_TF_OPS]
+    converter.experimental_new_converter = True
+
+    # 允许动态批次大小
+    converter._experimental_lower_tensor_list_ops = False
+
+    tflite_model = converter.convert()
+
+    # 运行 TFLite 计算
+    interpreter = tf.lite.Interpreter(model_content=tflite_model)
+    interpreter.allocate_tensors()
+
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    # 设置输入
+    interpreter.resize_tensor_input(
+        input_details[1]['index'], (11, 768))  # 例如 image_features 有 10 个
+    interpreter.allocate_tensors()
+
+
+    interpreter.set_tensor(
+        input_details[0]['index'], text_features.cpu().numpy().astype(np.float32))
+    interpreter.set_tensor(
+        input_details[1]['index'], image_features.cpu().numpy().astype(np.float32))
+
+    # 运行
+    interpreter.invoke()
+
+    # 获取输出
+    probs = interpreter.get_tensor(output_details[0]['index'])
+    print("TFLite Probabilities:", np.around(probs, 3))
